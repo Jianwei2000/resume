1d5c39c295605b141783bc08ff0bcf297ee63a3b
Jianwei <er891129@gmail.com>
2025-03-01 21:27:20 +0800

    作品集不錯

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -18,18 +18,26 @@ export default function Portfolio() {
             pin: true,
           },
         })
-        .fromTo(".portfolio-title",{
-            opacity:0
-        },{
-            y:-200,
-            opacity:1
-        })
-        .set(".grid-item", {
-          transformOrigin: "50% 0%",
-          z: () => gsap.utils.random(-5000, -2000),
-          filter: "brightness(0%)",
-          rotationX: () => gsap.utils.random(-65, -25),
-        },"<")
+        .fromTo(
+          ".portfolio-title",
+          {
+            opacity: 0,
+          },
+          {
+            y: -200,
+            opacity: 1,
+          }
+        )
+        .set(
+          ".grid-item",
+          {
+            transformOrigin: "50% 0%",
+            z: () => gsap.utils.random(-5000, -2000),
+            filter: "brightness(0%)",
+            rotationX: () => gsap.utils.random(-65, -25),
+          },
+          "<"
+        )
         .to(
           ".grid-item",
           {
@@ -46,6 +54,21 @@ export default function Portfolio() {
             z: 6500,
           },
           0
+        )
+        .fromTo(
+          ".wrap1",
+          {
+            x: 2000,
+          },
+          { x: -250 }
+        )
+        .fromTo(
+          ".wrap2",
+          {
+            x: -2200,
+          },
+          { x: 50 },
+          "<"
         );
     });
     return () => ctx.revert();
@@ -55,47 +78,75 @@ export default function Portfolio() {
     <>
       <section id="portfolio">
         <div className="portfolio-title">
-            作品集<span>Portfolio</span>
+          作品集<span>Portfolio</span>
         </div>
 
         <div className="grid-wrap">
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/01.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/02.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/03.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/04.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/05.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/06.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/07.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/08.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/09.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/10.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/11.PNG" alt="" />
           </div>
           <div className="grid-item">
-            <img src="banner-bg.jpg" alt="" />
+            <img src="portfolio/12.PNG" alt="" />
           </div>
-          
+         
+        </div>
+
+        <div className="flex-wrap">
+          <div class="wrap1">
+            <a href="https://jianwei2000.github.io/labor.github.io/jslearn/index.html" target="blink">
+              <img src="portfolio/07.PNG" alt="" />
+            </a>
+            <a href="https://jianwei2000.github.io/ToDoList/" target="blink">
+              <img src="portfolio/06.PNG" alt="" />
+            </a>
+            <a href="https://jianwei2000.github.io/Tea-Talk/#/" target="blink">
+              <img src="portfolio/01.PNG" alt="" />
+            </a>
+            <a href="https://jianwei2000.github.io/blog.github.io/Blog/index.html" target="blink">
+              <img src="portfolio/08.PNG" alt="" />
+            </a>
+          </div>
+          <div class="wrap2">
+            <a href="https://jianwei2000.github.io/SunHairSalon.github.io/SunHairSalon/designer.html" target="blink">
+              <img src="portfolio/10.PNG" alt="" />
+            </a>
+            <a href="https://jianwei2000.github.io/migrant/" target="blink">
+              <img src="portfolio/04.PNG" alt="" />
+            </a>
+            <a href="https://jianwei2000.github.io/DREAMBAR.github.io/JQueryPractice/index.html" target="blink">
+              <img src="portfolio/09.PNG" alt="" />
+            </a>
+          </div>
         </div>
       </section>
     </>
